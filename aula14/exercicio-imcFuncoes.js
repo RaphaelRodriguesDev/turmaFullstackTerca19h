4e702bdd68c5fad0b09459bbf0467931fa7fab14
Raphael Rodrigues <rars.developer@gmail.com>
2024-01-19 20:09:45 -0300

    Atualizando Arquivos

--- aula14/exercicio-imcFuncoes.js
+++ aula14/exercicio-imcFuncoes.js
@@ -15,15 +15,27 @@ IMC	Resultado
   --------------------------------------
 */
 
-let p = 60;
-let h = 1.7;
+let p = 53;
+let h = 1.69;
 
 const calcularIMC = (p, h) => p / (h * h);
 const IMC = calcularIMC(p, h);
 
 // crie a função chamada verificar peso
 const verificarPeso = imc => {
-  
+  if (imc < 18.5) {
+    console.log("Seu IMC está: ABAIXO DO PESO");
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    console.log("Seu IMC está: PESO NORMAL");
+  } else if (imc >= 25 && imc <= 29.9) {
+    console.log("Seu IMC está: SOBREPESO");
+  } else if (imc >= 30 && imc <= 34.9) {
+    console.log("Seu IMC está: OBESIDADE GRAU 1");
+  } else if (imc >= 35 && imc <= 39.9) {
+    console.log("Seu IMC está: OBESIDADE GRAU 2");
+  } else {
+    console.log("Seu IMC está: OBESIDADE GRAU 3");
+  }
 };
 
 verificarPeso(IMC);
